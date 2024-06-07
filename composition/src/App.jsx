baeef8f36b867a7dc0268f75f3ec0a44a5e548fc
jaly7 <160493864+jaly7@users.noreply.github.com>
2024-06-07 14:39:22 +0100

    Create App.jsx

---
+++ composition/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Container from './components/container'
+
+function App() {
+  return (
+    <>
+    <Container title="HELLO WORLD" children="CIAO MONDO"/>
+    </>
+  )
+}
+
+export default App
