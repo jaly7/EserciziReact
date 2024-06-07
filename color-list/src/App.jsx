3920d54c02b853edb368618fe27754b81478d951
jaly7 <160493864+jaly7@users.noreply.github.com>
2024-06-07 14:37:57 +0100

    Create App.jsx

---
+++ color-list/src/App.jsx
@@ -0,0 +1,16 @@
+// app.jsx
+import React from 'react';
+import Color from './components/color';
+
+
+const App = () => {
+  return (
+    <>
+      <Color />
+    </>
+  );
+};
+
+export default App;
+
+
