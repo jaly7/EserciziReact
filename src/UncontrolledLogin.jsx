a6c23e7b638d061434912d486b2b262da492326a
jaly7 <160493864+jaly7@users.noreply.github.com>
2024-06-07 14:18:35 +0100

    Create UncontrolledLogin.jsx

---
+++ src/UncontrolledLogin.jsx
@@ -0,0 +1,9 @@
+export const UncontrolledLogin = () => {
+    return(
+        <form>
+            <input name="username" type="text" />
+            <input name="password" type="password" />
+            <input name="remember" type="checkbox" />
+        </form>
+    )
+}
