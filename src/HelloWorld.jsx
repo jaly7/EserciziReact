67506d75f4697055ccac13be5620ecaec176c8bc
jaly7 <160493864+jaly7@users.noreply.github.com>
2024-06-07 14:18:22 +0100

    Create HelloWorld.jsx

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,20 @@
+import { createRoot } from "react-dom/client"
+
+export function HelloWorld() {
+    return (
+        <div>
+            <h2>Hello World</h2>
+        </div>
+    )
+}
+/*export function Apps(){
+    return(
+        <div>
+            <HelloWorld/>
+        </div>
+    )
+}
+createRoot(document.getElementById('root')).render(<Apps/>)*/
+
+
+
