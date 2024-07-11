314525707ca2ad9374959a231413b6da22906a16
jaly7 <160493864+jaly7@users.noreply.github.com>
2024-07-11 11:09:51 +0100

    Create AddUserMessage.jsx

---
+++ routers/src/components/AddUserMessage.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const AddUserMessage = () => {
+  return (
+    <div>
+      <p>Add a user and select it</p>
+    </div>
+  );
+};
+
+export default AddUserMessage;
