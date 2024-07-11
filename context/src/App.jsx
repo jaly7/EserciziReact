9eb6ea64272865139f6ae87e78c9317f2829f37d
jaly7 <160493864+jaly7@users.noreply.github.com>
2024-07-11 11:08:39 +0100

    Update App.jsx

--- context/src/App.jsx
+++ context/src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable no-unused-vars */
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css'
+import React, { useContext } from 'react';
+import { LanguageProvider, LanguageContext } from './components/languageContext';
+import Clock from './components/clock';
+
+const App = () => {
+  return (
+    <LanguageProvider>
+      <LanguageSelector />
+      <Clock />
+    </LanguageProvider>
+  );
+};
+
+const LanguageSelector = () => {
+  const { changeLanguage } = useContext(LanguageContext);
+
+  const handleLanguageChange = (e) => {
+    changeLanguage(e.target.value);
+  };
 
   return (
-    <>
-    
-    </>
-  )
-}
+    <select onChange={handleLanguageChange}>
+      <option value="en">English</option>
+      <option value="es">Español</option>
+      <option value="fr">Français</option>
+    </select>
+  );
+};
 
-export default App
+export default App;
